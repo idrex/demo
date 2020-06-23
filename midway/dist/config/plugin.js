961946adb81cb42931f2cc141dcd5aae2bcf91a5
Drex <drex@idrex.cn>
2020-06-23 20:26:44 +0800

    delete

--- midway/dist/config/plugin.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// had enabled by midway
-// export = {
-//   static: true,
-// };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGx1Z2luLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbmZpZy9wbHVnaW4udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLHdCQUF3QjtBQUN4QixhQUFhO0FBQ2Isa0JBQWtCO0FBQ2xCLEtBQUsifQ==
